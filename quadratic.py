884fe62f333b4873a365f080d30137f7f46ce2df
anasixto <162721927+anasixto@users.noreply.github.com>
2024-04-19 14:44:39 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -2,25 +2,24 @@ import math
 def roots(a, b, c):
     discriminante = b**2 - 4*a*c
     if discriminante > 0:
-        r1 = (-b + math.sqrt(discriminante))/2*a
-        r2 = (-b - math.sqrt(discriminante))/2*a
+        r1 = (-b + math.sqrt(discriminante))/(2*a)
+        r2 = (-b - math.sqrt(discriminante))/(2*a)
         return f"{r1}, {r2}"
-    elif discriminante == 0
-        r1 = (-b)/2*a
+    elif discriminante == 0:
+        r1 = (-b)/(2*a)
         return f"{r1}"
     else:
         return "()"
         
 
-
 def value_y(a, b, c, x):
-    y = a*(X**2)+b*X+c
+    y = a*(X**2) + b*x + c
     return f"{y}"
 
 
 def to_string(a, b, c):
-    return f"f(x) = {a} * (x**2) + {b}*X + {c}"
+    return f"f(x) = {a} * (x**2) + {b}*x + {c}"
 
 
 def derivation(a, b, c):
-    return f"f(x) = 2*{a}*X + {b}"
+    return f"f(x) = {2*a}*x + {b}"
